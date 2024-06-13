d02ed3f4442e9ea71c898995ed4108d6f9d4b711
noooooorrrrf <167782532+noooooorrrrf@users.noreply.github.com>
2024-06-13 21:10:52 +0300

    Update config.py

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Darksourcebot/CJ",
+    "https://github.com/noooooorrrrf/bot-mauzk",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
